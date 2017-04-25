24de968545453234d1f53c6d0060bbaa706724b9
Eusth <chikakarayattekita@yahoo.co.jp>
2017-04-25 20:19:22 +0200

    Introduce new helper class to detect if SteamVR is running.

---
+++ VRGIN/Helpers/SteamVRDetector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace VRGIN.Helpers
+{
+    public static class SteamVRDetector
+    {
+        private static bool FilterInvalidProcesses(Process p)
+        {
+            try
+            {
+                return p.ProcessName != null;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool IsRunning
+        {
+            get
+            {
+                return Process.GetProcesses().Where(FilterInvalidProcesses).Any(process => process.ProcessName == "vrcompositor");
+            }
+        }
+    }
+}
